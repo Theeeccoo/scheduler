4f3e180eec528529509ac2d847b6329bb691eaf2
Theeeccoo <thiagodecampos1@gmail.com>
2023-11-12 17:41:23 -0300

    Momentarily removing constant_workload

--- include/workload.h
+++ include/workload.h
@@ -25,6 +25,7 @@
 	#include <stdio.h>
 
 	#include "statistics.h"
+	#include "task.h"
 
 	/**
 	 * @brief Opaque pointer to a synthetic workload.
@@ -61,14 +62,15 @@
 	/**@{*/
 	extern workload_tt workload_create(histogram_tt, int, int);
 	extern void workload_destroy(workload_tt);
-	extern int workload_ntasks(const_workload_tt);
-	extern int workload_task(const_workload_tt, int);
+	extern int workload_ntasks(workload_tt);
+	extern int workload_task(workload_tt, int);
 	extern void workload_sort(workload_tt, enum workload_sorting);
-	extern int *workload_sortmap(const_workload_tt);
-	extern void workload_write(FILE *, const_workload_tt);
+	extern int *workload_sortmap(workload_tt);
+	extern void workload_write(FILE *, workload_tt);
 	extern workload_tt workload_read(FILE *);
 	extern void workload_set_task(workload_tt, int, int);
-	extern int *workload_cummulative_sum(const_workload_tt);
+	extern void workload_increase_elapsed_time(workload_tt, int, int);
+	extern int *workload_cummulative_sum(workload_tt);
 	/**@}*/
 
 #endif /* WORKLOAD_H_ */
